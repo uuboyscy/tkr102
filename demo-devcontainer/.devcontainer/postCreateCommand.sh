bafebd30b92fd3c8ba68cb3c7ce1185821785455
uuboyscy <aegis12321@gmail.com>
2026-08-09 11:32:40 +0800

    Added devcontainer demo; Added docker-compose.yaml

---
+++ demo-devcontainer/.devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
+&& sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
+
+# cp /root/.gitconfig_mounted /root/.gitconfig \
+# && cp -r /root/.ssh_mounted/* /root/.ssh/ \
+# && ls /root/.ssh/id_* | grep -v pub | xargs chmod 400 \
+# && chmod 755 /root/.ssh/config
+
+git config --global --add safe.directory /workspaces/*
