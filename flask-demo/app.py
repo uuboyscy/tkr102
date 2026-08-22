d39eba10051a97e90796c5671c9d39fe905678a4
uuboyscy <aegis12321@gmail.com>
2026-08-22 16:00:46 +0800

    feat: :sparkles: Add Flask app

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -6,5 +6,24 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello Flask!</h1>"
 
+@app.route("/api/employee/<dep_id>/<emp_id>")
+def get_employee(dep_id, emp_id):
+    sql = f"""
+        select
+            emp_name,
+            emp_id,
+            dep_id,
+            email
+        from staff
+        where dep_id = '{dep_id}' and emp_id = '{emp_id}'
+    """
+    # result = execute_sql(sql)
+    return {
+        "emp_name": "Allen",
+        "emp_id": emp_id,
+        "dep_id": dep_id,
+        "email": "aaa@example.com"
+    }
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=5001)
