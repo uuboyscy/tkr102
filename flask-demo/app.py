f5d1e818f9ea6c88f429dd8557670058744e7f5a
uuboyscy <aegis12321@gmail.com>
2026-08-23 10:34:12 +0800

    feat: :sparkles: Flask get post

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello Flask!</h1>"
 
+# GET /api/employee/<dep_id:str>/<emp_id:str>
 @app.route("/api/employee/<dep_id>/<emp_id>")
 def get_employee(dep_id, emp_id):
     sql = f"""
@@ -24,6 +25,38 @@ def get_employee(dep_id, emp_id):
         "dep_id": dep_id,
         "email": "aaa@example.com"
     }
+
+# GET /hello?username=Allen&age=22
+@app.route("/hello")
+def hello_user():
+    username = request.args.get("username")
+    age = request.args.get("age")
+    if not username:
+        return "Who are you?"
+    if not age:
+        return f"Hello {username}."
+    return f"Hello {username}, you are {age} years old."
+
+@app.route("/hello_post", methods=["GET", "POST"])
+def hello_post():
+    result_html = """
+    <html>
+    <form action="/hello_post" method="POST">
+        <label>What is your name?</label>
+        <br>
+        <input type="textbox" name="username">
+        <button type="submit">Submit</button>
+    </form>
+    <div>
+    %s
+    </div>
+    </html>
+    """
+    if request.method == "GET":
+        return result_html % ("")
+
+    username = request.form.get("username")
+    return result_html % ("Hello %s" % (username))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
